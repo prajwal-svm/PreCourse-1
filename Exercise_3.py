d4996b0c1159056569a2dacc935220f66c9078df
PrajwalS Venkateshmurthy <psv.globallyunique@gmail.com>
2025-01-18 17:54:50 -0800

    Exercise_3 : Implement Singly Linked List.

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,7 +3,9 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +19,52 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        new_node = ListNode(data)
         
+        # If the list is empty, make the new node the head
+        if self.head is None:
+            self.head = new_node
+            return
+            
+        # Traverse to the last node
+        current = self.head
+        while current.next:
+            current = current.next
+            
+        current.next = new_node
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        current = self.head
         
+        while current:
+            if current.data == key:
+                return current
+            current = current.next
+            
+        return None
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        # If list is empty
+        if not self.head:
+            return
+            
+        # If head node itself contains the key
+        if self.head.data == key:
+            self.head = self.head.next
+            return
+            
+        current = self.head
+        while current.next:
+            if current.next.data == key:
+                current.next = current.next.next
+                return
+            current = current.next
